6c6ee00da605fed7492b6865b64bb5823213a0d9
Afaan Bilal <afaanbilal@gmail.com>
2024-01-20 10:57:00 +0530

    ✨ Setup editor

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <script setup lang="ts">
+import { ref, shallowRef } from 'vue';
 import TopBar from './components/TopBar.vue';
+
+const code = ref(`
+wan("Hello, world!");
+`);
+const editorRef = shallowRef();
+
+function formatCode() {
+    editorRef.value?.getAction('editor.action.formatDocument').run()
+}
 </script>
 
 <template>
-    <div>
+    <div class="flex flex-col h-full">
         <TopBar />
 
-        <div class="flex h-full">
-            <div class="flex-1 border-r-2 border-r-slate-950">A</div>
-            <div class="flex-1">B</div>
+        <div class="flex flex-1">
+            <div class="self-stretch flex-1 border-r-2 border-r-slate-950">
+                <vue-monaco-editor ref="editorRef" v-model:value="code" theme="vs-dark" :options="{
+                    automaticLayout: true,
+                    formatOnType: true,
+                    formatOnPaste: true,
+                }" />
+            </div>
+            <div class="flex-1">
+                <pre>
+> Ready!
+                </pre>
+            </div>
+        </div>
+
+        <div class="flex p-1 bg-slate-950">
+            <div class="p-1 border" @click="formatCode">Format</div>
+            <div class="text-2xl">AAAAAAAAAAAAAA</div>
         </div>
     </div>
 </template>
-
-<style scoped>
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue';
 import './style.css';
 import App from './App.vue';
+import { install as VueMonacoEditorPlugin } from '@guolao/vue-monaco-editor'
 
-createApp(App).mount('#app');
+const app = createApp(App);
+
+app.use(VueMonacoEditorPlugin, {
+    paths: {
+        vs: 'https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs'
+    },
+});
+
+app.mount('#app');
